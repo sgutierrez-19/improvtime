995c199e1da91f75003d67e3dd9ab8d09f185015
Steven Gutierrez <stematgut@gmail.com>
2020-07-15 09:56:37 -0700

    Created axios route to hit /improv, created console log in main component to test it was working, and added cors filter in server to allow requests from localhost:3000

--- client/src/components/MainPage/index.tsx
+++ client/src/components/MainPage/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { getPrompt } from "../../api/improv";
 
 export class MainPage extends React.Component<any, any> {
+  async componentDidMount() {
+    let something = await getPrompt();
+    console.log(something);
+  }
   render() {
     return <>MAIN</>;
   }
--- server/src/main/java/com/revature/improv/ServerApplication.java
+++ server/src/main/java/com/revature/improv/ServerApplication.java
@@ -2,12 +2,30 @@ package com.revature.improv;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ServerApplication.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    // We're defining the class we're using inline here as a shortcut
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowCredentials(true)
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
+
+      }
+    };
+  }
 
 }
