f1aefb0186bc23a8df1edf1033c941fb7be947a6
Steven Gutierrez <stematgut@gmail.com>
2020-07-15 07:48:48 -0700

    added in bootstrap so I could import the css in the index.tsx

--- client/src/components/NavbarComp/index.tsx
+++ client/src/components/NavbarComp/index.tsx
@@ -1,7 +1,50 @@
 import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownItem,
+  DropdownMenu,
+  NavbarText,
+} from "reactstrap";
 
 export class NavbarComp extends React.Component<any, any> {
   render() {
-    return <>NAVBAR</>;
+    return (
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler />
+        <Collapse navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                GitHub
+              </NavLink>
+            </NavItem>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Options
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>Option 1</DropdownItem>
+                <DropdownItem>Option 2</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Reset</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    );
   }
 }
